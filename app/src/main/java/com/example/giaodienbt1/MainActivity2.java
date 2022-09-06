c123cbe8e1a80efb17886deb2ae63054e80ad9ca
KANIS <annguyenfc@gmail.com>
2022-09-06 10:20:47 +0700

    first commit

--- app/src/main/java/com/example/giaodienbt1/MainActivity2.java
+++ app/src/main/java/com/example/giaodienbt1/MainActivity2.java
@@ -2,13 +2,28 @@ package com.example.giaodienbt1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main2);
+
+        Button button = (Button) findViewById(R.id.btnback);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
